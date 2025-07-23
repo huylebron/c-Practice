1bbd3032a11c5b1d878e04baf0140b0eaaf56072
huylebron <doquochuy27102003@gmail.com>
2025-07-23 17:13:29 +0700

    LongestCommonPrefix

---
+++ c#LeetCode/Easy/Arr/LongestCommonPrefix.cs
@@ -0,0 +1,20 @@
+﻿string LongestCommonPrefix(string[] strs)
+{
+    if (strs.Length == 0) return "";
+
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.Length; i++)
+    {
+        while (!strs[i].StartsWith(prefix))
+        {
+            prefix = prefix[..^1];
+
+            if (prefix == "") return "";
+        }
+    }
+
+    return prefix;
+}
+
+Console.WriteLine(LongestCommonPrefix(new[] { "dog", "racecar", "car" }));    // Output: ""

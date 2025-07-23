aa0864230ad2d4c7a007544cf1bd377a89beb09c
huylebron <doquochuy27102003@gmail.com>
2025-07-23 14:20:05 +0700

    DuplicateZero leetcode

---
+++ c#LeetCode/Easy/Arr/DuplicateZero.cs
@@ -0,0 +1,35 @@
+﻿public class DuplicateZero
+{
+    public void DuplicateZeros(int[] arr)
+    {
+        int n = arr.Length;
+
+        int countZero = 0;
+        foreach (int x in arr)
+        {
+            if (x == 0) countZero++;
+        }
+
+        int i = n - 1;
+        int j = n + countZero - 1;
+
+        while (i >= 0)
+        {
+            if (j < n)
+            {
+                arr[j] = arr[i];
+            }
+
+            if (arr[i] == 0)
+            {
+                j--;
+                if (j < n)
+                {
+                    arr[j] = 0;
+                }
+            }
+            i--;
+            j--;
+        }
+    }
+}

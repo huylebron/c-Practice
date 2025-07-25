f56380a5aa76b8c77fa44e20092fc06bff4e8017
huylebron <doquochuy27102003@gmail.com>
2025-07-25 17:25:25 +0700

    Refactor RemoveElement class for readability and clarity
    Updated namespace formatting, simplified variable declarations, and streamlined logic in the RemoveElementimp method. Preserved functionality while enhancing code structure.

--- c#LeetCode/Easy/Arr/RemoveElement.cs
+++ c#LeetCode/Easy/Arr/RemoveElement.cs
@@ -1,28 +1,25 @@
-﻿namespace c_LeetCode.Easy.Arr
-{
-    public class RemoveElement
-    {
-        public int RemoveElementimp(int[] nums, int val)
-        {
-            int i = 0;
-            int j = nums.Length - 1;
+﻿namespace c_LeetCode.Easy.Arr;
 
-            while (i < j)
+public class RemoveElement
+{
+    public int RemoveElementimp(int[] nums, int val)
+    {
+        var i = 0;
+        var j = nums.Length - 1;
+
+        while (i < j)
+            if (nums[i] != val)
             {
-                if (nums[i] != val)
-                {
-                    i++;
-                }
-                else
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    j--;
-                }
+                i++;
+            }
+            else
+            {
+                var temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j--;
             }
 
-            return i;
-        }
+        return i;
     }
 }

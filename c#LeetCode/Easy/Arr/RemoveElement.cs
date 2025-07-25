0f8c8dba982e8de084f2377ca5852812a62748cd
huylebron <doquochuy27102003@gmail.com>
2025-07-25 17:24:17 +0700

    Add RemoveElement class with removal logic
    Introduced a new class `RemoveElement` in the `c_LeetCode.Easy.Arr` namespace. The class features the method `RemoveElementimp`, which utilizes a two-pointer technique to remove all instances of a specified value from an integer array, returning the new length of the array after the operation.

---
+++ c#LeetCode/Easy/Arr/RemoveElement.cs
@@ -0,0 +1,28 @@
+﻿namespace c_LeetCode.Easy.Arr
+{
+    public class RemoveElement
+    {
+        public int RemoveElementimp(int[] nums, int val)
+        {
+            int i = 0;
+            int j = nums.Length - 1;
+
+            while (i < j)
+            {
+                if (nums[i] != val)
+                {
+                    i++;
+                }
+                else
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    j--;
+                }
+            }
+
+            return i;
+        }
+    }
+}

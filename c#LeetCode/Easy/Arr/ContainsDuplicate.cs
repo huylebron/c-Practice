f837e2a9d8998bde43310085fc9cff2987fd4db8
huylebron <doquochuy27102003@gmail.com>
2025-07-26 11:59:45 +0700

    Add ContainsDuplicate class to check for duplicates
    Introduces the `ContainsDuplicate` class in the `c_LeetCode.Easy.Arr` namespace.
    The class includes the method `ContainsDuplicateimp`, which uses a `HashSet<int>`
    to efficiently determine if there are any duplicate integers in an input array `nums`.
    Returns `true` if duplicates are found, otherwise returns `false`.

---
+++ c#LeetCode/Easy/Arr/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿namespace c_LeetCode.Easy.Arr
+{
+    public class ContainsDuplicate
+    {
+        public bool ContainsDuplicateimp(int[] nums)
+        {
+            HashSet<int> result = new HashSet<int>();
+            // Bước 2: Duyệt qua từng số trong mảng nums
+            foreach (int num in nums)
+            {
+                if (result.Contains(num))
+                {
+                    return true;
+                }
+
+                result.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
